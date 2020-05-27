09a55de837745b9dd9be06c1db7efa82defbbb66
jie.fang <jie.fang@aispeech.com>
2020-05-27 14:08:05 +0800

    train

--- test.py
+++ test.py
@@ -20,9 +20,11 @@ labels=node.Node("label")
 c = mynet(shapes=(4,2))
 x = node.Node("x")
 loss = loss.cross_entropy(c(x),labels)
-a=np.array([2*np.ones(1*4),np.ones(1*4)])
-b=np.array([np.array([1,0]),np.array([0,1])])
-optimizer=opt.SGD(loss,c.parameters())
-optimizer.step(feed_dict={x:a,labels:b})
+a=[2*np.ones(1*4).reshape(1,4),np.ones(1*4).reshape(1,4)]
+b=[np.array([1,0]).reshape(1,2),np.array([0,1]).reshape(1,2)]
+for epoch in range(10):
+    for batch in range(2):
+        optimizer=opt.SGD(loss,c.parameters())
+        
+        optimizer.step(feed_dict={x:a[batch],labels:b[batch]})
 print(optimizer.parameters[1].const)
-#numpy.lib.stride_tricks.as_strided(x, shape=None, strides=None, subok=False, writeable=True)
