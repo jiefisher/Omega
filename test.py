54cdd5931350dc0611a05e37d5a99f416f2b7347
jie.fang <jie.fang@aispeech.com>
2020-05-27 14:03:14 +0800

    fcnet

--- test.py
+++ test.py
@@ -8,15 +8,21 @@ import module
 import linear
 import loss
 import opt
-
+class mynet(module.Module):
+    def __init__(self,shapes):
+        self.shapes = shapes
+        self.fc=linear.Linear(self.shapes)
+    def forward(self,x):
+        y=self.fc(x)
+        return y
 
 labels=node.Node("label")
-net = linear.Linear((4,2))
+c = mynet(shapes=(4,2))
 x = node.Node("x")
-loss = loss.cross_entropy(net(x),labels)
+loss = loss.cross_entropy(c(x),labels)
 a=np.array([2*np.ones(1*4),np.ones(1*4)])
 b=np.array([np.array([1,0]),np.array([0,1])])
-optimizer=opt.SGD(loss,net.parameters())
+optimizer=opt.SGD(loss,c.parameters())
 optimizer.step(feed_dict={x:a,labels:b})
 print(optimizer.parameters[1].const)
 #numpy.lib.stride_tricks.as_strided(x, shape=None, strides=None, subok=False, writeable=True)
